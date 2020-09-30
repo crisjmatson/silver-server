395db2e87a1ce5aad42d97bea8ba6ca1349a0f31
Cris Matson <crisjmatson@gmail.com>
2020-09-30 10:25:55 -0400

    db variable fix

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.NAME, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
 });
 /* const sequelize = new Sequelize(
